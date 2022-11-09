1cc2522c839bf0d2fb7035ba1ff3e32b40bdc391
Goultarde <117078126+Goultarde@users.noreply.github.com>
2022-11-09 20:40:12 +0100

    Update player.py
    On ne voit plus les coup du bot

--- Pierre_Papier_Ciseaux/player.py
+++ Pierre_Papier_Ciseaux/player.py
@@ -5,7 +5,7 @@ class Player :
 
     def ia(self): #Permet au bot de choisir une option entre pierre, papier et ciseaux
         choix = random.choice(["pierre","papier","ciseaux"])
-        print(choix)
+        
         return (choix)
         
 
@@ -14,7 +14,9 @@ class Player :
             choice = self.ia()
         else:#c'est un humain
             choice = input("Choisissez entre pierre, papier et ciseaux : ")
-            while choice not in ["pierre","papier","ciseaux"]:#Vérifie que le coup est valide
+            while choice not in ["pierre","papier","ciseaux"]: #Vérifie que le coup est valide
                 choice=input("ce choix n'existe pas, Choisissez entre pierre, papier et ciseaux : ")
         return(choice)
+
+    
 
